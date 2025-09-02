b38f98c462329ab4f2a6f261f4014a5347b7f0ab
Tahsina2226 <tahsinatanvin274@gmail.com>
2025-09-03 01:20:26 +0600

    basic setup

---
+++ src/app.ts
@@ -0,0 +1,18 @@
+import express, { Request, Response, NextFunction } from "express";
+import cors from "cors";
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req: Request, res: Response) => {
+  res.send("welcome to Student Life Toolkit");
+});
+
+app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
+  console.error(err.stack);
+  res.status(500).json({ message: err.message || "Server encountered an issue!" });
+});
+
+export default app;

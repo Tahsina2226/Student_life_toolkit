847dd86a481da83ca3a7c7f5fce61e79dbe01a86
Tahsina2226 <tahsinatanvin274@gmail.com>
2025-09-03 13:14:14 +0600

    budget tracker

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,9 @@
 import express, { Request, Response, NextFunction } from "express";
 import cors from "cors";
 import classRoutes from "../src/class_shedule/shedule.route";
+import incomeRoutes from "./budget-tracker/income/income.routes";
+import expenseRoutes from "./budget-tracker/expense/expense.routes";
+import summaryRoutes from "./budget-tracker/summary/summary.routes";
 
 const app = express();
 
@@ -12,7 +15,9 @@ app.get("/", (req: Request, res: Response) => {
 });
 
 app.use("/api/classes", classRoutes);
-
+app.use("/api/income", incomeRoutes);
+app.use("/api/expenses", expenseRoutes);
+app.use("/api/summary", summaryRoutes);
 app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
   console.error(err.stack);
   res

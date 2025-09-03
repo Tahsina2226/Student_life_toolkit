b6f49a69d84da6f7d9c741f7c0be29b16c90bf28
Tahsina2226 <tahsinatanvin274@gmail.com>
2025-09-03 10:35:57 +0600

    class shedule

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,6 @@
 import express, { Request, Response, NextFunction } from "express";
 import cors from "cors";
+import classRoutes from "../src/class_shedule/shedule.route";
 
 const app = express();
 
@@ -7,12 +8,16 @@ app.use(cors());
 app.use(express.json());
 
 app.get("/", (req: Request, res: Response) => {
-  res.send("welcome to Student Life Toolkit");
+  res.send("Welcome to Student Life Toolkit");
 });
+
+app.use("/api/classes", classRoutes);
 
 app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
   console.error(err.stack);
-  res.status(500).json({ message: err.message || "Server encountered an issue!" });
+  res
+    .status(500)
+    .json({ message: err.message || "Server encountered an issue!" });
 });
 
 export default app;
